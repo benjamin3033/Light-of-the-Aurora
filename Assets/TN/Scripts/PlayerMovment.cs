136d9e4ca4c496cec037de0008133bc62332668b
Tom <shyfoxsounds@gmail.com>
2021-10-19 15:05:09 +1300

    Player || Movment
    Movement and mouse look works with adjustable head bobbing

---
+++ Assets/TN/Scripts/PlayerMovment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public Vector3 move;
+    Vector3 velocity;
+    public bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        print(isGrounded);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+        move = move.normalized;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/TN/Scripts/headBobbing.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headBobbing : MonoBehaviour
+{
+    [SerializeField] private bool _enable = true;
+
+    [SerializeField, Range(0, 0.1f)] private float _amplitude = 0.015f;
+    [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
+
+    [SerializeField] private Transform _camera = null;
+    [SerializeField] private Transform _cameraHolder = null;
+
+    private float _toggleSpeed = 0.999f;
+    private Vector3 _startPos;
+    private CharacterController _controller;
+
+    PlayerMovment playermovment = null;
+
+    private void Awake()
+    {
+        _controller = gameObject.GetComponent<CharacterController>();
+        _startPos = _camera.localPosition;
+
+        playermovment = gameObject.GetComponent<PlayerMovment>();
+    }
+
+    void Update()
+    {
+        if (!_enable) return;
+
+        CheckMotion();
+        ResetPosition();
+        _camera.LookAt(FocusTarget());
+    }
+
+    private void PlayMotion(Vector3 motion)
+    {
+        print("PlayingMotion");
+        _camera.localPosition += motion;
+
+    }
+
+    private void CheckMotion()
+    {
+        float speed = new Vector3(playermovment.move.x, 0, playermovment.move.z).magnitude;
+
+        Debug.Log(speed);
+        if (speed < _toggleSpeed) return;
+        if (!playermovment.isGrounded) return;
+        print("Hit2");
+
+        PlayMotion(FootStepMotion());
+    }
+
+    private Vector3 FootStepMotion()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * _frequency) * _amplitude;
+        pos.x += Mathf.Cos(Time.time * _frequency / 2) * _amplitude * 2;
+        return pos;
+    }
+
+    private void ResetPosition()
+    {
+        if (_camera.localPosition == _startPos) return;
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 1 * Time.deltaTime);
+    }
+
+    private Vector3 FocusTarget()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
+        pos += _cameraHolder.forward * 15.0f;
+        return pos;
+    }
+
+
+}
